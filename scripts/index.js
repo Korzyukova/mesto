aa8cdfdbd652470e4bcf03373c43597d15f041a6
Tatiana Korzyukova <korzioukova@gmail.com>
2022-03-24 07:39:12 -0400

    24 March

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,24 @@
 
 
 let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_about')
+let jobInput = document.querySelector('.popup__input_type_about');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
+let placeName = document.querySelector('.popup__input_type_place-name');
+let linkName = document.querySelector('.popup__input_type_link');
 
-let placeNameInput = document.querySelector('.popup__input_type_place-name');
-let 
 
-const popup = document.querySelector('.popup');
-const openPopup = document.querySelector('.profile__edit-button');
-const closePopup = popup.querySelector('.popup__closed');
+
+const popup1 = document.querySelector('#popup1');
+const popup2 = document.querySelector('#popup2');
+const popup3 = document.querySelector('#popup3');
+const openPopup1 = document.querySelector('.profile__edit-button');
+const openPopup2 = document.querySelector('.profile__add-button');
+
+const closePopup1 = popup1.querySelector('.popup__closed');
+const closePopup2 = popup2.querySelector('.popup__closed');
+const closePopup3 = popup3.querySelector('.popup__closed');
+
 const initialCards = [
     {
         name: 'Архыз',
@@ -43,47 +51,76 @@ document.addEventListener("DOMContentLoaded", () => {
     let container = document.querySelector('.photo-grid');
 
     initialCards.forEach(function (item, index, array) {
-        let itemX = `<li class="photo-grid__rectangle" id="item_${index}">
-        <img class="photo-grid__item" src="${item.link}" alt="Старая церковь в горах">
+        let itemX = `<li class="photo-grid__rectangle">
+        <img onclick="deleteItem(this)"  src='./images/trash.png'/ class="trash">
+        <img class="photo-grid__item" src="${item.link}" alt="${item.name}">
+    
+        
         <div class="photo-grid__bottom">
             <h2 class="photo-grid__name">${item.name}</h2>
             <button class="photo-grid__heart"></button>
         </div>
-        <div onclick="deleteItem(${index})">DELETE</div>
+        
     </li>`
-            container.insertAdjacentHTML('afterbegin', itemX);
+        container.insertAdjacentHTML('afterbegin', itemX);
     })
 
+    const openPopup3 = document.querySelectorAll('.photo-grid__item');
+    openPopup3.forEach(function (item) {
+        item.addEventListener('click', () => {
+            togglePopup3(item)
+        });
+    });
 }
 
 );
 
-function deleteItem(index)
-{
-    let item = document.querySelector('#item_' + index);
+function deleteItem(target) {
     let container = document.querySelector('.photo-grid');
-    container.removeChild(item)
+    container.removeChild(target.parentNode)
 }
 
-function togglePopup() {
-    popup.classList.toggle('popup_opened');
+function togglePopup1() {
+    popup1.classList.toggle('popup_opened');
 }
 
+function togglePopup2() {
+    popup2.classList.toggle('popup_opened');
+};
 
-openPopup.addEventListener('click', () => {
-    togglePopup()
+function togglePopup3(target) {
+    popup3.classList.toggle('popup_opened');
+
+    document.querySelector('.popup__image').src = target.src;
+    document.querySelector('.popup__image_name').textContent = target.alt;
+}
+
+openPopup1.addEventListener('click', () => {
+    togglePopup1()
     nameInput.value = profileName.textContent
     jobInput.value = profileDescription.textContent
 });
+openPopup2.addEventListener('click', () => {
+    togglePopup2()
+
+    // placeName.value = profileName.textContent
+    // linkName.value = profileDescription.textContent
+});
 
 
-closePopup.addEventListener('click', togglePopup);
 
 
-let formElement = document.querySelector('.popup__container');
+
+closePopup1.addEventListener('click', togglePopup1);
+closePopup2.addEventListener('click', togglePopup2);
+closePopup3.addEventListener('click', togglePopup3);
+
+let formElement1 = document.querySelector('#form1');
+let formElement2 = document.querySelector('#form2');
+let formElement3 = document.querySelector('#form3');
 
 
-function formSubmitHandler(evt) {
+function formSubmitHandler1(evt) {
     console.log('event=', evt)
     evt.preventDefault();
     console.log('nameInput=', nameInput);
@@ -96,10 +133,30 @@ function formSubmitHandler(evt) {
 
     profileDescription.textContent = jobInput.value;
 
-    togglePopup();
+    togglePopup1();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler2(evt) {
+    evt.preventDefault();
+
+    let container = document.querySelector('.photo-grid');
+    let itemX = `<li class="photo-grid__rectangle">
+        <img class="photo-grid__item" src="${linkName.value}" alt="${placeName.value}">
+        <div class="photo-grid__bottom">
+            <h2 class="photo-grid__name">${placeName.value}</h2>
+            <button class="photo-grid__heart"></button>
+        </div>
+        <img onclick="deleteItem(this)" src='./images/trash.png' class="trash">
+    </li>`;
+
+    container.insertAdjacentHTML('afterbegin', itemX);
+
+    togglePopup2();
+}
+
+formElement1.addEventListener('submit', formSubmitHandler1);
+formElement2.addEventListener('submit', formSubmitHandler2);
 
 
 
