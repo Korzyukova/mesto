9f504b6edb40d4abfd5b513dcefcab7c67f2ff5c
Tatiana Korzyukova <korzioukova@gmail.com>
2022-04-13 13:12:52 -0400

    13 april

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,103 +1,100 @@
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
 
- const inputSelector = document.querySelectorAll('.popup__input');
+const enableValidation = (settings) => {
+    console.log('enableValidation')
+    // Найдём все формы с указанным классом в DOM,
+    // сделаем из них массив методом Array.from
+    const formList = Array.from(document.querySelectorAll('.popup__container'));
+  //console.log('enableValidation', formList)
+    // Переберём полученную коллекцию
+    formList.forEach((formElement) => {
+      formElement.addEventListener('submit', (evt) => {
+        // У каждой формы отменим стандартное поведение
+        evt.preventDefault();
+      });
+  
+      // Для каждой формы вызовем функцию setEventListeners,
+      // передав ей элемент формы
+      setEventListeners(formElement, settings);
+    });
+};
 
- // Функция, которая добавляет класс с ошибкой
- const showInputError = (element) => {
-     element.classList.add('popup__input_type_error');
- 
-     const formErrorSpan = document.querySelector(`.${element.id}-error`);
-     formErrorSpan.classList.add('span_show');
- 
-     const nameInputElement = document.getElementById('nameInput')
-     const aboutInputElement = document.getElementById('aboutInput')
-     const placeInputElement = document.getElementById('placeInput')
-     const linkInputElement = document.getElementById('linkInput')
- 
-     const isNameInputValid = nameInputElement.validity.valid;
-     const isAboutInputValid = aboutInputElement.validity.valid;
-     const isPlaceInputValid = placeInputElement.validity.valid;
-     const isLinkInputValid = linkInputElement.validity.valid;
- 
-     if (!isNameInputValid || !isAboutInputValid) {
-         document.querySelector('#submitProfile').disabled = true;
-     }
- 
-     if (!isPlaceInputValid || !isLinkInputValid) {
-         document.querySelector('#submitCard').disabled = true;
-     }
- };
- 
- // Функция, которая удаляет класс с ошибкой
- const hideInputError = (element) => {
- 
-     element.classList.remove('popup__input_type_error');
- 
-     const formErrorSpan = document.querySelector(`.${element.id}-error`);
-     formErrorSpan.classList.remove('span_show');
- 
-     const nameInputElement = document.getElementById('nameInput')
-     const aboutInputElement = document.getElementById('aboutInput')
-     const placeInputElement = document.getElementById('placeInput')
-     const linkInputElement = document.getElementById('linkInput')
- 
-     const isNameInputValid = nameInputElement.validity.valid;
-     const isAboutInputValid = aboutInputElement.validity.valid;
-     const isPlaceInputValid = placeInputElement.validity.valid;
-     const isLinkInputValid = linkInputElement.validity.valid;
- 
-     if ((isNameInputValid && isAboutInputValid) || (isPlaceInputValid && isLinkInputValid)) {
-         const buttons = document.querySelectorAll('.popup__submit');
-         buttons.forEach(function (item) {
-             item.disabled = false;
-         });
-     }
- };
- 
- // Функция, которая проверяет валидность поля
- const isValid = (event) => {
-     console.log(event);
-     if (!event.target.validity.valid) {
-         // Если поле не проходит валидацию, покажем ошибку
-         showInputError(event.target);
-     } else {
-         // Если проходит, скроем
-         hideInputError(event.target);
-     }
- };
- 
- formElementProfile.addEventListener('submit', function (evt) {
-     // Отменим стандартное поведение по сабмиту
- 
-     let valid = true;
- 
-     inputSelector.forEach(function (item) {
- 
-         if (!item.validity.valid) {
-             valid = false;
-         }
-     });
- 
-     if (valid) {
-         profileName.textContent = nameInput.value;
-         profileDescription.textContent = jobInput.value;
-         closePopup(profilePopup);
-     }
- 
- 
-     evt.preventDefault();    
- });
- 
- 
- // Вызовем функцию isValid на каждый ввод символа
- 
- 
- inputSelector.forEach(function (item) {
-     item.addEventListener('input', isValid);
- });
- 
- 
- const formInput = formElementProfile.querySelector('.popup__input');
- const formError = formElementProfile.querySelector(`.${nameInput.id}_error`);
+// фукция enable/disable Save button
+const enableDisableSaveButton = (buttonElement, inputList) => {
+  if (inputList.some(el => !el.validity.valid)) {
+    // если есть хотя бы одно поле, не прошедшее валидацию, кнопка будет неактивна
+    console.log('disable buton')
+    buttonElement.disabled = true;
+  } else {
+    console.log('enable button')
+    buttonElement.disabled = false;
+  }
+}
 
- 
- 
+const setEventListeners = (formElement, settings) => {
+    // Находим все поля внутри формы,
+    // сделаем из них массив методом Array.from
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__submit')
+  //console.log('setEventListeners', inputList)
+    // Обойдём все элементы полученной коллекции
+    inputList.forEach((inputElement) => {
+    //console.log('inputElement=', inputElement)
+      // каждому полю добавим обработчик события input
+      inputElement.addEventListener('input', () => {
+        // Внутри колбэка вызовем isValid,
+        // передав ей форму и проверяемый элемент
+        isValid(formElement, inputElement);
+        enableDisableSaveButton(buttonElement, inputList)
+      });
+    });
+};
+  
+const showInputError = (formElement, inputElement, errorMessage) => {
+    // Находим элемент ошибки внутри самой функции
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    console.log('showInputError: errorElement=', errorElement)
+    // Остальной код такой же
+    inputElement.classList.add('form__input_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('form__input-error_active');
+  };
+  
+  const hideInputError = (formElement, inputElement) => {
+    // Находим элемент ошибки
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    console.log('hideInputError: errorElement=', errorElement)
+    // Остальной код такой же
+    inputElement.classList.remove('form__input_type_error');
+    errorElement.classList.remove('form__input-error_active');
+    errorElement.textContent = '';
+  };
+
+enableValidation({
+  formSelector: '.popup__container',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}); 
+
+
+
+// Функция isValid теперь принимает formElement и inputElement,
+// а не берёт их из внешней области видимости
+
+const isValid = (formElement, inputElement) => {
+    console.log(formElement, inputElement)
+    if (!inputElement.validity.valid) {
+      // showInputError теперь получает параметром форму, в которой
+      // находится проверяемое поле, и само это поле
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      // hideInputError теперь получает параметром форму, в которой
+      // находится проверяемое поле, и само это поле
+      hideInputError(formElement, inputElement);
+    }
+  };
+
